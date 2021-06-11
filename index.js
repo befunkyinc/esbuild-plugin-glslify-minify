84351388cf67ebfabe231c4d41b57cf7d03c261e
Micah Engle-Eshleman <micah@befunky.com>
2021-06-11 15:02:20 -0700

    Commit basic functionality
    Based on the following packages:
    - darionco/esbuild-plugin-glslify
    - leosingleton/webpack-glsl-minify/

---
+++ index.js
@@ -0,0 +1,86 @@
+/**
+ * Based on https://github.com/glslify/rollup-plugin-glslify
+ */
+'use strict';
+
+import { dirname } from 'path';
+import { promises as fsPromises } from 'fs';
+import * as _glslify from 'glslify';
+import GlslMinify from './lib/minify.js';
+
+let GlslMinifyInstance;
+async function compressShader(code) {
+  // Code adapted from https://github.com/leosingleton/webpack-glsl-minify
+  GlslMinifyInstance = GlslMinifyInstance || new GlslMinify();
+  return await GlslMinifyInstance.executeFile({ contents: code }).sourceCode;
+}
+
+const kDefaultConfig = {
+  extensions: [
+    'vs',
+    'fs',
+    'vert',
+    'frag',
+    'glsl',
+  ],
+  compress: false,
+};
+
+function createFilter(extensions) {
+  let expression = `\\.(?:${extensions.map(e => e.split('.').filter(s => Boolean(s)).join('\\.')).join('|')
+    })$`;
+  return new RegExp(expression);
+}
+
+function glslify(options) {
+  const config = Object.assign({}, kDefaultConfig, options);
+  const filter = createFilter(config.extensions);
+
+  let requireIndex = 0;
+
+  return {
+    name: 'glslify',
+    setup(build) {
+      // unfortunately glslify is not async
+      build.onLoad({ filter }, async (args) => {
+        const contents = await fsPromises.readFile(args.path, 'utf8');
+
+        const fileOptions = Object.assign({
+          basedir: dirname(args.path),
+        }, config);
+
+        const code = _glslify.default.compile(contents, fileOptions);
+
+        console.log(requireIndex);
+
+        if (config.compress) {
+          if (requireIndex === 0) {
+            console.log('Compressing...', args.path);
+            // console.log('Before', code);
+          }
+
+          code = await compressShader(code);
+
+          if (requireIndex === 0) {
+            // console.log('After', code);
+          }
+        }
+
+        // if (contents.includes('require(')) {
+        //     if (requireIndex === 0) {
+        //         console.log('contents:\n', contents);
+        //         console.log('code:\n', code);
+        //     }
+        requireIndex++;
+        // }
+
+        return {
+          contents: code,
+          loader: 'text',
+        };
+      });
+    }
+  }
+}
+
+export { glslify, glslify as default };
